d659739981e667ee515e6cc0b0e66d632c773dd1
Ross O'Reilly <oreillyross@gmail.com>
2016-05-17 14:51:47 +0000

    first failing test

--- inventory/test.js
+++ inventory/test.js
@@ -1,4 +1,9 @@
-
+import tape from 'tape'
+import updateInventory from './updateInventory'
 
 // updateInventory() should return an array.
+
+test('should return an array', expect => {
+    expect.fail('I should fail')
+})
 
